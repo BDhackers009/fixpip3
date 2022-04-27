24bd48159a497945f04259e63e5e58a9c098517c
BDhaCkers009 <ahmedshifat1234@gmail.com>
2022-04-27 20:51:40 +0000

    added py3.10 support

--- fix_pip.sh
+++ fix_pip.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 fakpip() {
-folder1="/usr/lib/python3/dist-packages/pip/_vendor/platformdirs"
-folder2="/usr/local/lib/python3.9/dist-packages/pip/_vendor/platformdirs"
+path1="/usr/lib/python3/dist-packages/pip/_vendor/platformdirs"
+path4="/usr/lib/python3.10/dist-packages/pip/_vendor/platformdirs"
+path2="/usr/local/lib/python3.9/dist-packages/pip/_vendor/platformdirs"
+path3="/usr/local/lib/python3.10/dist-packages/pip/_vendor/platformdirs"
 URL="https://raw.githubusercontent.com/BDhackers009/fixpip3/main"
-if [[ -d ${folder1} ]]; then
-	rm -rf ${folder1}/android.py
-	rm -rf ${folder1}/__init__.py
-	wget -qO ${folder1}/android.py ${URL}/android.py
-	wget -qO ${folder1}/__init__.py ${URL}/__init__.py
+if [[ -d ${path1} ]]; then
+	rm -rf ${path1}/android.py
+	rm -rf ${path1}/__init__.py
+	wget -qO ${path1}/android.py ${URL}/android.py
+	wget -qO ${path1}/__init__.py ${URL}/__init__.py
 fi
-if [[ -d ${folder2} ]]; then
-	rm -rf ${folder2}/android.py
-        rm -rf ${folder2}/__init__.py
-        wget -qO ${folder2}/android.py ${URL}/android.py
-        wget -qO ${folder2}/__init__.py ${URL}/__init__.py
+if [[ -d ${path2} ]]; then
+	rm -rf ${path2}/android.py
+        rm -rf ${path2}/__init__.py
+        wget -qO ${path2}/android.py ${URL}/android.py
+        wget -qO ${path2}/__init__.py ${URL}/__init__.py
+fi
+if [[ -d ${path3} ]]; then
+        rm -rf ${path3}/android.py
+        rm -rf ${path3}/__init__.py
+        wget -qO ${path3}/android.py ${URL}/android.py
+        wget -qO ${path3}/__init__.py ${URL}/__init__.py
+fi
+if [[ -d ${path4} ]]; then
+        rm -rf ${path4}/android.py
+        rm -rf ${path4}/__init__.py
+        wget -qO ${path4}/android.py ${URL}/android.py
+        wget -qO ${path4}/__init__.py ${URL}/__init__.py
 fi
 }
 if [[ $(command -v pip3) ]]; then
